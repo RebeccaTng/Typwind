9131ed05be47979862c328ccc3c8608c31cf92d1
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2022-11-26 16:41:53 +0100

    Deleted unreviewed files

--- app/Views/exercisesContent.php
+++
@@ -1,9 +0,0 @@
-<section>
-    <h2>Welcome to the <?=$title?>page</h2>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Assumenda consectetur ea earum est labore minima perspiciatis repellendus rerum,
-        voluptate voluptates? Aliquam, blanditiis, doloribus.
-        Modi nobis numquam quam reiciendis soluta. Recusandae.
-    </p>
-</section>
