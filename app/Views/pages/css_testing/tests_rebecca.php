b5fd6917881559913bcd7d19704cce0ece1183b3
Rebecca Tang <rebecca.tang@student.kuleuven.be>
2022-11-29 22:12:45 +0100

    made buttons CSS

--- app/Views/pages/css_testing/tests_rebecca.php
+++ app/Views/pages/css_testing/tests_rebecca.php
@@ -4,18 +4,25 @@
     <!--START OF PAGE CONTENT-->
     <!--ADD HERE ALL THE PHP AND HTML THAT YOUR PAGE NEEDS-->
 
-    <h1>Title</h1>
-    <h2>Heading 1</h2>
-    <h3>Heading 2</h3>
-    <h4>Heading 4</h4>
-    <p class="smallText">Small text</p>
-    <p class="bodyExText">Body explanatory text</p>
-    <p class="bodyText">Body text</p>
-    <p class="emphasisExText">Emphasis explanatory text</p>
-    <p class="emphasisText">Emphasis text</p>
-    <p class="subTitle">Subtitle</p>
-    <p class="navText">Navigation text</p>
-    <p class="navEmphasisText">Navigation emphasis text</p>
+    <!-- SYSTEM DESIGN -->
+    <!--    <h1>Title</h1>-->
+    <!--    <h2>Heading 1</h2>-->
+    <!--    <h3>Heading 2</h3>-->
+    <!--    <h4>Heading 4</h4>-->
+    <!--    <p class="smallText">Small text</p>-->
+    <!--    <p class="bodyExText">Body explanatory text</p>-->
+    <!--    <p class="bodyText">Body text</p>-->
+    <!--    <p class="emphasisExText">Emphasis explanatory text</p>-->
+    <!--    <p class="emphasisText">Emphasis text</p>-->
+    <!--    <p class="subTitle">Subtitle</p>-->
+    <!--    <p class="navText">Navigation text</p>-->
+    <!--    <p class="navEmphasisText">Navigation emphasis text</p>-->
+
+    <!-- BUTTONS -->
+    <button class="button buttonPrimary buttonExpert">Button Label</button>
+    <button class="button buttonSecondary buttonExpert">Button Label</button>
+    <button class="button buttonPrimary buttonChild">Button Label</button>
+    <button class="button buttonSecondary buttonChild">Button Label</button>
 
     <!--END OF PAGE CONTENT-->
 <?= $this->endSection() ?>
