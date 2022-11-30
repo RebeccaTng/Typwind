de65df11fbe16561d5aac27908b040b1447c18eb
Rebecca Tang <rebecca.tang@student.kuleuven.be>
2022-11-30 14:19:32 +0100

    added scrollbar CSS

--- app/Views/pages/css_testing/tests_rebecca.php
+++ app/Views/pages/css_testing/tests_rebecca.php
@@ -5,18 +5,18 @@
     <!--ADD HERE ALL THE PHP AND HTML THAT YOUR PAGE NEEDS-->
 
     <!-- SYSTEM DESIGN -->
-    <!--    <h1>Title</h1>-->
-    <!--    <h2>Heading 1</h2>-->
-    <!--    <h3>Heading 2</h3>-->
-    <!--    <h4>Heading 4</h4>-->
-    <!--    <p class="smallText">Small text</p>-->
-    <!--    <p class="bodyExText">Body explanatory text</p>-->
-    <!--    <p class="bodyText">Body text</p>-->
-    <!--    <p class="emphasisExText">Emphasis explanatory text</p>-->
-    <!--    <p class="emphasisText">Emphasis text</p>-->
-    <!--    <p class="subTitle">Subtitle</p>-->
-    <!--    <p class="navText">Navigation text</p>-->
-    <!--    <p class="navEmphasisText">Navigation emphasis text</p>-->
+    <h1>Title</h1>
+    <h2>Heading 1</h2>
+    <h3>Heading 2</h3>
+    <h4>Heading 4</h4>
+    <p class="smallText">Small text</p>
+    <p class="bodyExText">Body explanatory text</p>
+    <p class="bodyText">Body text</p>
+    <p class="emphasisExText">Emphasis explanatory text</p>
+    <p class="emphasisText">Emphasis text</p>
+    <p class="subTitle">Subtitle</p>
+    <p class="navText">Navigation text</p>
+    <p class="navEmphasisText">Navigation emphasis text</p>
 
     <!-- BUTTONS -->
     <button class="button buttonPrimary buttonExpert">Button Label</button>
