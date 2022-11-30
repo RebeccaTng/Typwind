91f709ad66a3d109b29aa7ff7992c127ba4768f3
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2022-11-30 13:31:15 +0100

    Add experts folder

---
+++ app/Views/pages/experts/home_content.php
@@ -0,0 +1,19 @@
+
+<?= $this->extend('/templates/experts_default') ?>
+
+<?= $this->section('content') ?>
+<h3 class="text-center">
+    <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle">
+</h3>
+<section>
+    <h2 class="text-center">Welcome Back,</h2> <br>
+    <h3 class="text-center">
+        Emily Wauters <?php
+        $session = session();
+        echo $session->get('name')
+        ?>
+    </h3>
+
+</section>
+<?= $this->endSection() ?>
+
