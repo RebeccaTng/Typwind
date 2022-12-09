aaa019ba4593e07c1410336b17e0157128275f24
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2022-12-09 16:17:40 +0100

    fix navigation

--- app/Views/templates/experts_default.php
+++ app/Views/templates/experts_default.php
@@ -1,52 +1,28 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="<?=base_url()?>/public/CSS/navbar.css">
-    <!-- Dropdown menu for Expert exercises CSS -->
-    <link rel="stylesheet" href="<?=base_url()?>/public/CSS/dropDownExpertExercises.css">
-    <!-- jQuery CDN -->
-    <script src="https://code.jquery.com/jquery-1.12.0.min.js" defer></script>
-    <!-- Bootstrap Js CDN -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" defer></script>
-    <!-- sidebar Toggle Js -->
-    <script type="text/javascript" src="<?=base_url()?>/public/js/sidebarToggle.js" defer></script>
-    <!-- Language switch Js -->
     <script type="text/javascript" src="<?=base_url()?>/public/js/languageData.js" defer></script>
-    <!-- Scrolling in exercise page Js -->
-    <script type="text/javascript" src="<?=base_url()?>/public/js/ScrollExercises.js" defer></script>
-
-
 
     <!--CSS FILES-->
     <link rel="stylesheet" href="<?=base_url()?>/public/CSS/main.css">
+    <link rel="stylesheet" href="<?=base_url()?>/public/CSS/menubar.css">
     <link rel="stylesheet" href="<?=base_url()?>/public/CSS/generalComponents.css">
-</head>
-<body>
-<div>
-    <!-- Sidebar Holder -->
-    <?= $this->include('templates/side_nav_bar') ?>
-    <!-- Page Content Holder -->
-    <div id="content">
-        <button type="button" id="sidebarCollapse">
-            <span></span>
-            <span></span>
-            <span></span>
-        </button>
 
-        <main>
-            <?= $this->renderSection('content') ?>
-        </main>
+    <title>Typewind Online</title>
+</head>
+<body >
+<div class="grid-container">
+    <div class="leftNavBar">
+        <?= $this->include('templates/side_nav_bar') ?>
+    </div>
+    <div class="mainContent ">
+        <?= $this->renderSection('content') ?>
     </div>
 </div>
-
-
 </body>
 </html>
--- app/Views/templates/side_nav_bar.php
+++ app/Views/templates/side_nav_bar.php
@@ -1,38 +1,37 @@
-<nav id="sidebar">
-    <div class="menu_grid-container">
-        <div class="menuHeader">
-            <img  src="<?=base_url()?>/public/assets/general/typwind_logo.svg" width="100%" height="70">
 
-        </div>
-        <div class=" menuSubHeader">
-            <a href="#" language='nederlands' class="active">NED</a>
-            |
-            <a href="#" language='english'>ENG</a>
-        </div>
-        <div class="menuItems" >
-            <a href="<?=base_url()?>/experts/home"  class="home" >
-                <img  src="<?=base_url()?>/public/assets/icons/Home_icon.svg" width="20px" height="20px">
-                Home
-            </a>
-            <a href="<?=base_url()?>/experts/studentsList"  class="students">
-                <img  src="<?=base_url()?>/public/assets/icons/Students_Icon.svg" width="20px" height="20px">
-                Students
-            </a>
-            <a href="<?=base_url()?>/experts/exercises"  class="exercises">
-                <img  src="<?=base_url()?>/public/assets/icons/Students_Icon.svg" width="20px" height="20px">
-                Exercises
-            </a>
-            <a href="<?=base_url()?>/experts/profile" >
-                <img  src="<?=base_url()?>/public/assets/icons/profile_icon.svg" width="20px" height="20px">
-                    My Profile
-            </a>
-        </div >
-        <div  class="menuFooter">
-            <a href="<?=base_url()?>/registration/welcome" title="Go home" class="download">
-                <img  src="<?=base_url()?>/public/assets/icons/log_out_icon.svg" width="20px" height="20px">
-                Log out
-            </a>
-        </div>
+<div class="menu_grid-container">
+    <div class="menuHeader">
+        <img  src="<?=base_url()?>/public/assets/general/typwind_logo.svg" width="100%" height="70">
+
     </div>
+    <div class=" menuSubHeader">
+        <a href="#" language='nederlands' class="active">NED</a>
+        |
+        <a href="#" language='english'>ENG</a>
+    </div>
+    <div class="menuItems" >
+        <a href="<?=base_url()?>/experts/home"  class="home" >
+            <img  src="<?=base_url()?>/public/assets/icons/Home_icon.svg" >
+            Home
+        </a>
+        <a href="<?=base_url()?>/experts/studentsList"  class="students">
+            <img  src="<?=base_url()?>/public/assets/icons/Students_Icon.svg" >
+            Students
+        </a>
+        <a href="<?=base_url()?>/experts/exercises"  class="exercises">
+            <img  src="<?=base_url()?>/public/assets/icons/Students_Icon.svg" >
+            Exercises
+        </a>
+        <a href="<?=base_url()?>/experts/profile" >
+            <img  src="<?=base_url()?>/public/assets/icons/profile_icon.svg" >
+                My Profile
+        </a>
+    </div >
+    <div  class="menuFooter">
+        <a href="<?=base_url()?>/registration/welcome" title="Go home" class="download">
+            <img  src="<?=base_url()?>/public/assets/icons/log_out_icon.svg" >
+            Log out
+        </a>
+    </div>
+</div>
 
-</nav>
