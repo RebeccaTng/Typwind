0bf05a801905a239d142bfe069244f92c26e64c8
alexandria.bohn <alexandria.bohn@student.kuleuven.be>
2022-12-09 01:58:18 +0100

    Created the welcome page. When merging the nav bar needs to be omitted!

--- app/Views/pages/css_testing/welcome.php
+++ app/Views/pages/css_testing/welcome.php
@@ -1,27 +1,24 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title> Welcome Page</title>
-</head>
-<body>
-<h2 class="text-center">Typwind</h2>
-<h3 class="text-center">Welcomes you!</h3>
 
-<h5 class="text-center">Are you a student or a teacher?</h5>
+<?= $this->extend('/templates/css_default') ?>
 
-<div class="d-grid">
+<?= $this->section('content') ?>
+<!--START OF PAGE CONTENT-->
+<!--ADD HERE ALL THE PHP AND HTML THAT YOUR PAGE NEEDS-->
 
-    <button id="teacher" onclick="window.location= '<?=base_url()?>/registration/expertLogin'" title="Look at profile" class="btn btn-dark">TEACHER</button>
+<div class = "card_welcome_page">
+
+    <img src="/public/assets/general/Typwind_logo_editable.svg" alt="Typwind logo" style="width:221px;height:77px;">
+    <h1 style="color:var(--blueNeutral);">Welcomes you!</h1>
+    <h3 style="color:var(--primary-darkest);">Are you a student or a teacher?</h3>
+
+    <div class="teacher_student_buttons">
+        <button class="button buttonExpert buttonPrimary" onclick="window.location= '<?=base_url()?>/registration/expertLogin'" title="Look at profile" class="btn btn-dark">TEACHER</button>
+        <button class="button buttonExpert buttonPrimary" onclick="window.location= '<?=base_url()?>/registration/studentLogin'" class="btn btn-dark">STUDENT</button>
+    </div>
+
 </div>
 
-<br>
 
-<div class="d-grid">
-    <button id="student" onclick="window.location= '<?=base_url()?>/registration/studentLogin'" class="btn btn-dark">STUDENT</button>
-</div>
 
-</body>
-</html>
+<!--END OF PAGE CONTENT-->
+<?= $this->endSection() ?>
--- app/Views/templates/css_default.php
+++ app/Views/templates/css_default.php
@@ -28,6 +28,7 @@
     <link rel="stylesheet" href="<?=base_url()?>/public/CSS/child_components_varia.css">
     <link rel="stylesheet" href="<?=base_url()?>/public/assets/icons/uicons-bold-rounded/css/uicons-bold-rounded.css">
     <link rel="stylesheet" href="<?=base_url()?>/public/assets/icons/uicons-solid-rounded/css/uicons-solid-rounded.css">
+    <link rel="stylesheet" href="<?=base_url()?>/public/CSS/welcome.css">
 
 </head>
 <body>
