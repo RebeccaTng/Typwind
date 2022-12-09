fe7bbce4b88c6dc7fe3f6716d63aa190d76a8a38
Stefan Vanhille <Stefan.vanhille@hotmail.com>
2022-12-09 12:30:29 +0100

    Commenting out unnecesary code (will be used by Loïc later)

--- app/Views/pages/kids/home.php
+++ app/Views/pages/kids/home.php
@@ -2,11 +2,11 @@
 
 <?= $this->section('content') ?>
 
-<section>
+<!--<section>
     <h1>Select exercise:</h1>
 
 
-    <form action="<?php echo base_url('kids/intro');?>" method="get">
+    <form action="<?php /*echo base_url('kids/intro');*/?>" method="get">
         <label for="idExercises">id exercise:</label><br>
         <input type="text" id="idExercises" name="idExercises"><br>
         <input type="submit" value="Submit">
@@ -16,14 +16,15 @@
 <section>
     <h1>Go to feedback page:</h1>
 
-    <form action="<?php echo base_url('kids/feedback');?>" method="get">
+    <form action="<?php /*echo base_url('kids/feedback');*/?>" method="get">
         <label for="idExercises">id exercise:</label><br>
         <input type="text" id="idExercises" name="idExercises"><br>
         <input type="submit" value="Submit">
     </form>
 <h3 class="text-center">
     <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" class="rounded-circle">
-</h3>
+</h3>-->
+
 <section>
     <h2>Welcome Back</h2>
     <h3>
