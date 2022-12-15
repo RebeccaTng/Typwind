74bba1003dedbdcfa1264f18a9f195b7fd15d042
alexandria.bohn <alexandria.bohn@student.kuleuven.be>
2022-12-15 20:54:48 +0100

    Made the welcome page responsive

--- app/Views/pages/registration/welcome.php
+++ app/Views/pages/registration/welcome.php
@@ -10,17 +10,19 @@
 </head>
 <body>
 
-<div class = "card_welcome_page">
+<div class = "grid-container_welcome">
+    <div class = "card_welcome_page">
 
-    <img src="/public/assets/general/typwind_logo.svg" alt="Typwind logo" style="width:221px;height:77px;">
-    <h1 style="color:var(--blueNeutral);">Welcomes you!</h1>
-    <h3 style="color:var(--primary-darkest);">Are you a student or a teacher?</h3>
+        <img src="/public/assets/general/typwind_logo.svg" alt="Typwind logo" style="width:221px;height:77px;">
+        <h1 style="color:var(--blueNeutral); margin-bottom: 15px;">Welcomes you!</h1>
+        <h3 style="color:var(--primary-darkest);">Are you a student or a teacher?</h3>
 
-    <div class="teacher_student_buttons">
-        <button class="button buttonExpert buttonPrimary" onclick="window.location= '<?=base_url()?>/registration/expertLogin'" title="Look at profile" >TEACHER</button>
-        <button class="button buttonExpert buttonPrimary" onclick="window.location= '<?=base_url()?>/registration/studentLogin'" >STUDENT</button>
+        <div class="teacher_student_buttons">
+            <button class="button buttonExpert buttonPrimary" onclick="window.location= '<?=base_url()?>/registration/expertLogin'" title="Look at profile" >TEACHER</button>
+            <button class="button buttonExpert buttonPrimary" onclick="window.location= '<?=base_url()?>/registration/studentLogin'" >STUDENT</button>
+        </div>
+
     </div>
-
 </div>
 
 </body>
