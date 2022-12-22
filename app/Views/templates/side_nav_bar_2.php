29c993289d6b2620fc7911306fb36f91524c8c36
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2022-12-22 12:14:44 +0100

    display correct elements in NavBar kids

--- app/Views/templates/side_nav_bar_2.php
+++ app/Views/templates/side_nav_bar_2.php
@@ -1,21 +1,26 @@
 
 
-<nav id="sidebar">
-    <div class="sidebar-header">
-        <h3>Typwind</h3>
-        <div class="langWrap">
-            <a id="<?php echo $_COOKIE["nederlandsActief"];?>" href="#" language='nederlands' class="active">NED</a>
-            |
-            <a id="<?php echo $_COOKIE["englishActive"];?>" href="#" language='english' >ENG</a>
-        </div>
+<div class="menu_grid-container">
+    <div class="menuHeader">
+        <img  src="<?=base_url()?>/public/assets/general/typwind_logo.svg" width="100%" height="70">
     </div>
+    <div class="langWrap">
+        <a id="<?php echo $_COOKIE["nederlandsActief"];?>" href="#" language='nederlands' class="active">NED</a>
+        |
+        <a id="<?php echo $_COOKIE["englishActive"];?>" href="#" language='english' >ENG</a>
+    </div>
+    <div class="menuItems" >
+        <!--        <img  src="--><?php //=base_url()?><!--/public/assets/icons/Home_icon.svg" >-->
+        <a href="<?=base_url()?>/kids/home" title="Go home" class="home" >Home</a>
+        <!--        <img  src="--><?php //=base_url()?><!--/public/assets/icons/Students_Icon.svg" >-->
+        <a href="<?=base_url()?>/kids/exercises" title="Go to exercises" class="exercises">Exercises</a>
+        <!--        <img  src="--><?php //=base_url()?><!--/public/assets/icons/Students_Icon.svg" >-->
 
-    <ul class="list-unstyled components">
-        <li><a href="<?=base_url()?>/kids/home" title="Go home" class="home" >Home</a></li>
-        <li><a href="<?=base_url()?>/kids/exercises" title="Go to exercises" class="exercises">Exercises</a></li>
-    </ul>
-
-    <ul class="list-unstyled CTAs">
-        <li><a href="<?=base_url()?>/registration/welcome" title="Log Out" class="download">Log Out</a></li>
-    </ul>
-</nav>
+    </div >
+    <div  class="menuFooter">
+        <a href="<?=base_url()?>/registration/welcome" title="Go home" class="download">
+            <img  src="<?=base_url()?>/public/assets/icons/log_out_icon.svg" >
+            Log out
+        </a>
+    </div>
+</div>
