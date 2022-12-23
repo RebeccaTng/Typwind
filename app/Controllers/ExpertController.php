c987dd5c67c7988193b959cee1ffffd01e7e4e32
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2022-12-23 01:25:53 +0100

    fix dynamic loading of addSttudentPage

--- app/Controllers/ExpertController.php
+++ app/Controllers/ExpertController.php
@@ -80,7 +80,7 @@ class ExpertController extends BaseController
                 return $this->includeCSSFilesInCommonFiles( $this->studentOverview);
             case 'editStudentPage':
                 return $this->includeCSSFilesInCommonFiles( $this->editStudentPage);
-            case 'addStudent':
+            case 'addStudentPage':
                 return $this->includeCSSFilesInCommonFiles( $this->addStudentPage);
             case 'profile':
                 return $this->includeCSSFilesInCommonFiles( $this->profile);
