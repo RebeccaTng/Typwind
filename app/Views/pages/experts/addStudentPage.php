efc47dc51872bb2516aaa3eb178424cfa4bacd5f
Rebecca Tang <rebecca.tang@student.kuleuven.be>
2022-12-23 01:53:03 +0100

    CSS for add new student done

--- app/Views/pages/experts/addStudentPage.php
+++ app/Views/pages/experts/addStudentPage.php
@@ -9,44 +9,54 @@
 
     <h1>Add Student</h1>
 
-    <form action= "<?php echo base_url('experts/addStudent');?>" method="post">
+    <div class="scroller">
+        <form class="studentContainer" action= "<?php echo base_url('experts/addStudent');?>" method="post">
+            <div class="general card">
+                <h3>General Information</h3>
+                <p>
+                    <label for="firstname"><b>First name:</b></label>
+                    <input type="text" id="firstname" name="firstname" placeholder="First name"><br>
+                    <label for="lastname"><b>Surname:</b></label>
+                    <input type="text" id="lastname" name="lastname" placeholder="Surname"><br>
+                    <label for="email"><b>Email:</b></label>
+                    <input type="text" id="email" name="email" placeholder="Email"><br>
 
-        <label for="firstname">firstname:</label>
-        <input type="text" id="firstname" name="firstname"><br><br>
-        <label for="lastname">lastname:</label>
-        <input type="text" id="lastname" name="lastname"><br><br>
-        <label for="email">email:</label>
-        <input type="text" id="email" name="email"><br><br>
-        <label for="password">password:</label>
-        <input type="text" id="password" name="password"><br><br>
+                    <label for="gender"><b>Gender:</b></label>
+                    <select name="gender" id="gender">
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select><br>
 
-        <label for="gender">Gender:</label>
-        <select name="gender" id="gender">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select><br><br>
+                    <label for="birthday"><b>Birthday:</b></label>
+                    <input type="date" id="birthday" name="birthday"><br>
 
-        <label for="birthday">birthday:</label>
-        <input type="date" id="birthday" name="birthday"><br><br>
+                    <label for="teachers"><b>Teacher:</b></label>
+                    <select name="teachers" id="teachers" value="<?=$teachers[0]->firstname?>">
+                        <?php foreach ($teachers as $teacher):?>
+                            <option value="<?=$teacher->idTeachers?>"> <?=$teacher->firstname?> </option>
+                        <?php endforeach;?>
+                    </select><br>
 
-        <label for="teachers">Teacher:</label>
-        <select name="teachers" id="teachers" value="<?=$teachers[0]->firstname?>">
-            <?php foreach ($teachers as $teacher):?>
-                <option value="<?=$teacher->idTeachers?>"> <?=$teacher->firstname?> </option>
-            <?php endforeach;?>
-        </select><br><br>
+                    <label for="handSelection"><b>Hand Selection:</b></label>
+                    <select name="handSelection" id="handSelection">
+                        <option value="One Hand, right hand">Right Hand</option>
+                        <option value="One Hand, left hand">Left Hand</option>
+                        <option value="Both Hands">Both Hands</option>
+                    </select><br>
 
-        <label for="handSelection">Hand Selection:</label>
-        <select name="handSelection" id="handSelection">
-            <option value="One Hand, right hand">One Hand, right hand</option>
-            <option value="One Hand, left hand">One Hand, left hand</option>
-            <option value="Both Hands">Both Hands</option>
-        </select><br><br>
+                    <label for="active"><b>Active</b></label>&nbsp;
+                    <label class="switch">
+                        <input type="checkbox" id="active" name="active">
+                        <span class="slider"></span>
+                    </label>
 
-        <label for="active">Active</label>
-        <input type="checkbox" id="active" name="active"<br><br>
+                </p>
+            </div>
 
-        <input type="submit" value="Add student">
-    </form>
+            <div class="bottomBar">
+                <input type="submit" value="Add student" class="button buttonPrimary buttonExpert">
+            </div>
+        </form>
+    </div>
 
 <?= $this->endSection() ?>
