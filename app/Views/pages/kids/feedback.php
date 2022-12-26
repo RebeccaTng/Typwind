2ce939a8137e5a0dce898999ff820d97a4232ba0
loicr <loic.rondou@student.kuleuven.be>
2022-12-26 12:17:47 +0100

    added breadcrumbs

--- app/Views/pages/kids/feedback.php
+++ app/Views/pages/kids/feedback.php
@@ -4,10 +4,14 @@
 <div class = "grid-container_feedback">
     <div class = "breadcrumb">
         <ul class="breadcrumb">
-            <li><a href="#">Page 1</a></li>
-            <li><a href="#">Page 2</a></li>
-            <li><a href="#">Page 3</a></li>
-            <li>Page 4</li>
+            <li><a href="<?php echo base_url('/kids/exercises');?>">Exercises</a>
+            <li><a href="<?php echo base_url('/kids/intro/'.$idExercises);?>">Introduction</a></li>
+            <?php foreach ($exercises as $ex):?>
+                <?php  if ($ex->idExercises==$idExercises):?>
+                    <li><?= $ex->name?></li>
+                <?php endif;?>
+            <?php endforeach;?>
+            <li>Feedback</li>
         </ul>
     </div>
     <div class="title" style="color: var(--blueNeutral)">
