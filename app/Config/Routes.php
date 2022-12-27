6c448c0701fe487ce09a98f8273d020775c8fec1
Stefan Vanhille <Stefan.vanhille@hotmail.com>
2022-12-27 12:50:57 +0100

    Active language as bold

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -57,7 +57,6 @@ $routes->post('/experts/editProfile/(:num)', 'ExpertController::editProfile/$1',
 
 /// Kids Routes
 $routes->get('/kids/(:any)', 'KidsController::view/$1',['filter'=>'AuthGuard']);
-//test 123
 $routes->get('/kids/home', 'KidsController::home',['filter'=>'AuthGuard']);
 $routes->get('/kids/exercises', 'KidsController::exercises',['filter'=>'AuthGuard']);
 $routes->get('/kids/intro/(:num)', 'KidsController::intro/$1',['filter'=>'AuthGuard']);
