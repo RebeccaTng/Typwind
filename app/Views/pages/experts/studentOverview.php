2ac69132e7db9a5833f674c5fee30760d6f31569
Rebecca Tang <rebecca.tang@student.kuleuven.be>
2022-12-30 18:06:13 +0100

    Changed studentoverview and edit page

--- app/Views/pages/experts/studentOverview.php
+++ app/Views/pages/experts/studentOverview.php
@@ -7,68 +7,67 @@
 
         <ul class="breadcrumb">
             <li><a href="<?php echo base_url('/experts/studentsList');?>">Students</a></li>
-            <li><?=$person->firstname?><?=$person->lastname?></li>
+            <li><?=$person->firstname?> <?=$person->lastname?></li>
         </ul>
 
-        <h1><?=$person->firstname?><?=$person->lastname?></h1>
+        <h1><?=$person->firstname?> <?=$person->lastname?></h1>
 
         <div class="scroller">
             <div class="studentContainer">
-                <div class="general card">
-                    <h3>General Information</h3>
-                    <div class="roundProfilePic">
-                        <img src="/public/assets/avatars/1.svg" alt="User Icon">
-                    </div>
-                    <?php if ($person->handSelection==1):?>
-                        <img class="hands" src="<?php echo base_url('/public/assets/general/hands_right.svg');?>" alt="Italian Trulli">
-                    <?php endif;?>
-                    <?php if ($person->handSelection==2):?>
-                        <img class="hands" src="<?php echo base_url('/public/assets/general/hands_left.svg');?>" alt="Italian Trulli">
-                    <?php endif;?>
-                    <?php if ($person->handSelection==0):?>
-                        <img class="hands" src="<?php echo base_url('/public/assets/general/hands_both.svg');?>" alt="Italian Trulli">
-                    <?php endif;?>
-
-                    <p>
-                        <b>First name:</b>&nbsp&nbsp<?= $person->firstname?><br>
-                        <b>Surname:&nbsp&nbsp</b><?= $person->lastname?><br>
-                        <b>Email:&nbsp&nbsp</b><?= $person->email?><br>
-                        <b>Gender:&nbsp&nbsp</b>
-                        <?php if ($person->gender==1):?>
-                            Male
-                        <?php endif;?>
-                        <?php if ($person->gender==0):?>
-                            Female
-                        <?php endif;?>
-                        <br>
-                        <b>Birthday:</b>&nbsp&nbsp<?= $person->birthday?><br>
-                        <b>Teacher:</b>&nbsp&nbsp<?= $person->teacherFirstname?><br>
-                        <b>Hand Selection:&nbsp&nbsp</b>
-                        <?php if ($person->handSelection==1):?>
-                            Right Hand
-                        <?php endif;?>
-                        <?php if ($person->handSelection==2):?>
-                            Left Hand
-                        <?php endif;?>
-                        <?php if ($person->handSelection==0):?>
-                            Both Hands
-                        <?php endif;?>
-                        <br>
-                        <b>Active:&nbsp&nbsp</b>
-                        <?php if ($person->isActive==1):?>
-                            Currently Active
-                        <?php endif;?>
-                        <?php if ($person->isActive==0):?>
-                            Not Active
-                        <?php endif;?>
-                        <br>
-                    </p>
+                <div class="roundProfilePic">
+                    <img src="/public/assets/avatars/1.svg" alt="User Icon">
                 </div>
+                <?php if ($person->handSelection==1):?>
+                    <img class="hands" src="<?php echo base_url('/public/assets/general/hands_right.svg');?>" alt="Italian Trulli">
+                <?php endif;?>
+                <?php if ($person->handSelection==2):?>
+                    <img class="hands" src="<?php echo base_url('/public/assets/general/hands_left.svg');?>" alt="Italian Trulli">
+                <?php endif;?>
+                <?php if ($person->handSelection==0):?>
+                    <img class="hands" src="<?php echo base_url('/public/assets/general/hands_both.svg');?>" alt="Italian Trulli">
+                <?php endif;?>
 
-                <div class="noteCard card">
-                    <h3>Notes</h3>
+                <div class="infoContainer">
+                    <div class="general">
+                        <h3>General Information</h3>
+                        <p>
+                            <b>First name:</b>&nbsp&nbsp<?= $person->firstname?><br>
+                            <b>Surname:&nbsp&nbsp</b><?= $person->lastname?><br>
+                            <b>Email:&nbsp&nbsp</b><?= $person->email?><br>
+                            <b>Gender:&nbsp&nbsp</b>
+                            <?php if ($person->gender==1):?>
+                                Male
+                            <?php endif;?>
+                            <?php if ($person->gender==0):?>
+                                Female
+                            <?php endif;?>
+                            <br>
+                            <b>Birthday:</b>&nbsp&nbsp<?= $person->birthday?><br>
+                            <b>Teacher:</b>&nbsp&nbsp<?= $person->teacherFirstname?><br>
+                            <b>Hand Selection:&nbsp&nbsp</b>
+                            <?php if ($person->handSelection==1):?>
+                                Right Hand
+                            <?php endif;?>
+                            <?php if ($person->handSelection==2):?>
+                                Left Hand
+                            <?php endif;?>
+                            <?php if ($person->handSelection==0):?>
+                                Both Hands
+                            <?php endif;?>
+                            <br>
+                            <b>Active:&nbsp&nbsp</b>
+                            <?php if ($person->isActive==1):?>
+                                Currently Active
+                            <?php endif;?>
+                            <?php if ($person->isActive==0):?>
+                                Not Active
+                            <?php endif;?>
+                            <br>
+                        </p>
+                    </div>
                     <div class="notes">
-                        <p><?=$person->notes ?>
+                        <h3>Notes</h3>
+                        <p class="noteText"><?=$person->notes ?></p>
                     </div>
                 </div>
             </div>
