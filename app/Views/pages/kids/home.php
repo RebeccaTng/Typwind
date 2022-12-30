a6fe4ef0f41a9b01b2507bee256d863b0cf01223
alexandria.bohn <alexandria.bohn@student.kuleuven.be>
2022-12-30 22:12:03 +0100

    Fixed button layout, added card and dynamic positioning

--- app/Views/pages/kids/home.php
+++ app/Views/pages/kids/home.php
@@ -3,48 +3,46 @@
 <?= $this->section('content') ?>
 
 <div class="home_content">
-    <div>
-        <h1 style="color:var(--blueNeutral);" class="two"> Welcome Back </h1>
-        <h1 style="color:var(--blueNeutral); ">
-            <?php
-            $session = session();
-            echo $session->firstname; echo "!";
-            ?>
-        </h1>
+    <h1 style="color:var(--blueNeutral);" class="two"> Welcome Back </h1>
+    <h1 style="color:var(--blueNeutral); text-transform: capitalize; ">
+        <?php
+        $session = session();
+        echo $session->firstname; echo "!";
+        ?>
+    </h1>
 
-    </div>
-    <div>
+    <div class="card_home_child">
         <h3 style="color:var(--blueNeutral);" class="three"> Your next exercise is waiting for you!</h3>
-    </div>
 
-<!--    <div class="start_ex_button">-->
-    <?php foreach ($exercises as $ex):
-        $found =0;
-        foreach ($scores as $sc):
-            if (($ex->idExercises== $sc->idExercise_fk) && ($sc->idStudent_fk == $idStudents)):
-                $found=1;
-                break;
-            endif;
-        endforeach;
+    <!--    <div class="start_ex_button">-->
+        <?php foreach ($exercises as $ex):
+            $found =0;
+            foreach ($scores as $sc):
+                if (($ex->idExercises== $sc->idExercise_fk) && ($sc->idStudent_fk == $idStudents)):
+                    $found=1;
+                    break;
+                endif;
+            endforeach;
 
-        if($found == 0): ?>
-        <div class="start_ex_button">
-                <p class="exercise_name" style="color:var(--blueNeutral); text-transform: none;"> <?=$ex->name;?></p>
-        </div>
-        <form action="<?php echo base_url('/kids/intro/'.$ex->idExercises);?>" class="start_ex_button" >
-            <button class="button buttonPrimary buttonChild four" >Start new exercise</button>
-        </form>
-        <?php break;
-        endif;
-        $found =1;
-    endforeach;
-        if($found ==1): ?>
-            <button class="button buttonSecondary buttonChild five" style="color:var(--blueNeutral); text-transform: none;">Congratulations, you have finished the game!</button>
-            <form action="<?php echo base_url('/kids/intro/1');?>" class="inline">
-                <button class="button buttonPrimary buttonChild four">Start new exercise</button>
+            if($found == 0): ?>
+            <div class="start_ex_button">
+                    <h4 class="exercise_name" style="color:var(--primary-dark); text-transform: none;"> <?=$ex->name;?></h4>
+            </div>
+            <form action="<?php echo base_url('/kids/intro/'.$ex->idExercises);?>" class="start_ex_button" >
+                <button class="button buttonPrimary buttonChild four" >Start new exercise</button>
             </form>
-        <?php endif;?>
-<!--    </div>-->
+            <?php break;
+            endif;
+            $found =1;
+        endforeach;
+            if($found ==1): ?>
+                <button class="button buttonSecondary buttonChild five" style="color:var(--blueNeutral); text-transform: none;">Congratulations, you have finished the game!</button>
+                <form action="<?php echo base_url('/kids/intro/1');?>" class="inline">
+                    <button class="button buttonPrimary buttonChild four">Start new exercise</button>
+                </form>
+            <?php endif;?>
+    <!--    </div>-->
+    </div>
 
 </div>
 
