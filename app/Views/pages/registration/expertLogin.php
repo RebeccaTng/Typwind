5cc05de138edc802393961cdde50dcc14f7257f4
Rebecca Tang <rebecca.tang@student.kuleuven.be>
2023-01-01 22:09:45 +0100

    Added CSS to language and error messages on welcome, logins and registe

--- app/Views/pages/registration/expertLogin.php
+++ app/Views/pages/registration/expertLogin.php
@@ -19,30 +19,31 @@
 </head>
 
 <body>
-    <div class="card">
-        <div class="langWrap menuSubHeader" >
-            <a id="<?php echo $_COOKIE["nederlandsActief"];?>" href="#" language='nederlands' class="active">NED</a>
-            |
+    <div class="centering">
+        <div class="card">
+            <?php $session=session();
+            $session->set('isLoggedIn',False);
+            ?>
+            <h1>Login</h1>
+            <p class="one">Please log in with your email</p>
+            <?php if(session()->getFlashdata('msg')):?>
+                <div class="errorMessage">
+                    <?= session()->getFlashdata('msg') ?>
+                </div>
+            <?php endif;?>
+
+            <form action="<?php echo base_url(); ?>/RegistrationController/loginExpert" method="post">
+                <input type="email" name="email" placeholder="Email" value="<?php if (isset($_COOKIE["expertEmail"])) echo $_COOKIE["expertEmail"]; ?>" required>
+                <input id="JSPass" type="password" name="password" placeholder="Password" value="">
+                <button type="submit" class="button buttonPrimary buttonExpert">LOGIN</button>
+            </form>
+            <button id="REGISTER" onclick="window.location= '<?=base_url()?>/registration/register'" class="button buttonSecondary buttonExpert two">REGISTER</button>
+        </div>
+
+        <div class="langWrap menuSubHeader language" >
+            <a id="<?php echo $_COOKIE["nederlandsActief"];?>" href="#" language='nederlands' class="active">NED</a> |
             <a id="<?php echo $_COOKIE["englishActive"];?>" href="#" language='english' >ENG</a>
         </div>
-        <?php $session=session();
-        $session->set('isLoggedIn',False);
-        ?>
-        <h1>Login</h1>
-        <p class="one">Please log in with your email</p>
-        <?php if(session()->getFlashdata('msg')):?>
-            <div>
-                <?= session()->getFlashdata('msg') ?>
-            </div>
-        <?php endif;?>
-
-        <form action="<?php echo base_url(); ?>/RegistrationController/loginExpert" method="post">
-            <input type="email" name="email" placeholder="Email" value="<?php if (isset($_COOKIE["expertEmail"])) echo $_COOKIE["expertEmail"]; ?>" required>
-            <input id="JSPass" type="password" name="password" placeholder="Password" value="">
-            <button type="submit" class="button buttonPrimary buttonExpert">LOGIN</button>
-        </form>
-        <button id="REGISTER" onclick="window.location= '<?=base_url()?>/registration/register'" class="button buttonSecondary buttonExpert two">REGISTER</button>
-
     </div>
     <img src="/public/assets/general/typwind_logo_white.svg" alt="Typwind Logo" class="logo">
 </body>
