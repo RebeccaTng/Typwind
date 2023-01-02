df768546d2229ea5bc0e6cd029e0dd1e00053c1c
Patricio Castillo <patricioadolfo.castillocalderon@student.kuleuven.be>
2023-01-02 23:03:26 +0100

    load avatars using model

--- app/Models/Avatars.php
+++ app/Models/Avatars.php
@@ -6,31 +6,33 @@ use CodeIgniter\Model;
 
 class Avatars extends Model
 {
-    private array $avatarIcons;
+    private array $avatarIcons=array();
     protected $db;
+    protected $DBGroup = 'default';
     protected int $idOfSelectedAvatar;
     protected array $avatars;
     protected array $avatarsBought;
+    const  BOUGHT_CSS_CLASS = 'bought';
+    const  SELECTED_CSS_CLASS = 'chosen';
 
 
 
-    public function __construct($idStudent)
+    public function getAvatarIcons($idStudent): array
     {
         $this->avatars=$this->geAvatars();
         $this->avatarsBought=$this->geAvatarsBought($idStudent);
         if (! empty($this->avatars)):
             $this->setIdOfSelectedAvatar();
-            $this->setAvatarIcons($idStudent);
+            $this->setAvatarIcons();
         endif;
-    }
-
-
-    public function getAvatarIcons(): array
-    {
         return $this->avatarIcons;
     }
 
 
+    public function getIdOfSelectedAvatar(): int
+    {
+        return $this->idOfSelectedAvatar;
+    }
     public function setIdOfSelectedAvatar():void
     {
         $this->idOfSelectedAvatar=1;
@@ -43,9 +45,25 @@ class Avatars extends Model
             endforeach;
         endif;
     }
-    public function setAvatarIcons($idStudent): void
+    public function setAvatarIcons(): void
     {
-
+        $avatarIcons=array();
+        for($i=0;$i<count($this->avatars);$i++){
+            $avatarIcons[$i] = array('idAvatars' => $this->avatars[$i]->idAvatars, 'classCSS' => 'avatarChoice', 'price' =>  $this->avatars[$i]->price);
+            if (! empty($this->avatarsBought)){
+                foreach ($this->avatarsBought as $avatarBought){
+                    if($avatarBought->idAvatar_fk== $this->avatars[$i]->idAvatars||$this->avatars[$i]->idAvatars==1){
+                        $avatarIcons[$i]['classCSS']= 'avatarChoice'.' '.self::BOUGHT_CSS_CLASS;
+                        if ($avatarBought->selected){
+                            $avatarIcons[$i]['classCSS']= 'avatarChoice'.' '.self::SELECTED_CSS_CLASS;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        unset($this->avatarIcons);
+        $this->avatarIcons= $avatarIcons;
     }
     private function geAvatars():array
     {
--- app/Views/pages/kids/avatar.php
+++ app/Views/pages/kids/avatar.php
@@ -6,63 +6,28 @@
 
     <h1>My Avatar</h1>
     <div class="avatarContent">
-            <?php if (! empty($avatars) && is_array($avatars)):
-                $idOfSelectedAvatar=1;
-                if (! empty($avatarsBought) && is_array($avatarsBought)):
-                    foreach ($avatarsBought as $avatarBought):
-                        if($avatarBought->selected):
-                            $idOfSelectedAvatar=$avatarBought->idAvatar_fk;
-                            break;
-                        endif;
-                    endforeach;
-                endif;?>
             <div class="roundProfilePic currentPic">
+                <?php if (! empty($idOfSelectedAvatar)):?>
                 <img src="/public/assets/avatars/<?=$idOfSelectedAvatar?>.svg" alt="User Icon">
+                <?php endif;?>
             </div>
-
             <div class="coins">
                 <h3>My Coins</h3>
                 <span class="coin">120</span>
             </div>
             <div class="card avatarCard">
-            <?php foreach ($avatars as $avatar):
-                        $selected=0;
-                        $bought = 0;
-                        if (! empty($avatarsBought) && is_array($avatarsBought)):
-                            foreach ($avatarsBought as $avatarBought):
-                                if($avatarBought->idAvatar_fk== $avatar->idAvatars):
-                                    $bought = 1;
-                                    if ($avatarBought->selected):
-                                        $selected = 1;;
-                                    endif;
-                                    break;
-                                endif;
-                            endforeach;
-                        endif;
-                        if($idOfSelectedAvatar==$avatar->idAvatars):?>
-                            <div class="avatarChoice chosen"> <!--EVERY AVATAR SHOULD HAVE 'avatarChoice'-->
-                                <div class="roundProfilePic">
-                                    <img src="/public/assets/avatars/<?= $avatar->idAvatars?>.svg" alt="User Icon">
-                                </div>
-                                <span class="coin"></span>
-                            </div>
-                        <?php elseif($bought ||$avatar->idAvatars==1):?>
-                            <div class="avatarChoice bought"> <!--ADD 'bought' TO DIV IF IT IS THEY BOUGHT THE AVATAR ALREADY BUT IT IS NOT THE CURRENT ONE-->
-                                <div class="roundProfilePic">
-                                    <img src="/public/assets/avatars/<?= $avatar->idAvatars?>.svg" alt="User Icon">
-                                </div>
-                                <span class="coin"></span>
-                            </div>
-                        <?php else:?>
-                            <div class="avatarChoice "> <!--EVERY AVATAR SHOULD HAVE 'avatarChoice'-->
-                                <div class="roundProfilePic">
-                                    <img src="/public/assets/avatars/<?= $avatar->idAvatars?>.svg" alt="User Icon">
-                                </div>
-                                <span class="coin"><?= $avatar->price?></span>
-                            </div>
-                    <?php endif;?>
-                <?php endforeach;?>
-            <?php endif;?>
+
+            <?php if (! empty($avatars) && is_array($avatars)):
+                foreach ($avatars as $avatar):?>
+                    <div class="<?= $avatar['classCSS']?>">
+                        <div class="roundProfilePic">
+                            <img src="/public/assets/avatars/<?= $avatar ['idAvatars']?>.svg" alt="User Icon">
+                        </div>
+                        <span class="coin"><?= $avatar['price']?></span>
+                    </div>
+                <?php endforeach;
+            endif;?>
+
         </div>
     </div>
     <!--END OF PAGE CONTENT-->
