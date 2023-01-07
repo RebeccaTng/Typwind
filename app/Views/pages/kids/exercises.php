37271d4543ce841b5228c863e856781b6ddc508b
Stefan Vanhille <Stefan.vanhille@hotmail.com>
2023-01-07 20:59:01 +0100

    Making the Arrow Pictures Work

--- app/Views/pages/kids/exercises.php
+++ app/Views/pages/kids/exercises.php
@@ -130,12 +130,12 @@
 <h1 style="user-select: none;" class="one">Exercises</h1>
 <div class="card_and_button">
 
-    <button class="button round" style="background-color: transparent; user-select: none; border: none" onclick="document.querySelector('.exerciseContainer').scrollBy(0, -100)"><img alt="Arrow Up Icon" src="/public/assets/icons/up.png"></button>
+    <button class="button round" style="background-color: transparent; user-select: none; border: none" onclick="document.querySelector('.exerciseContainer').scrollBy(0, -100)"><img alt="Arrow Up Icon" src="/public/assets/icons/up.svg"></button>
 
     <div class="exerciseContainer">
     </div>
 
-    <button class="button round" style="background-color: transparent; user-select: none; border: none" onclick="document.querySelector('.exerciseContainer').scrollBy(0, 100)"><img alt="Arrow Down Icon" src="/public/assets/icons/down.png"></button>
+    <button class="button round" style="background-color: transparent; user-select: none; border: none" onclick="document.querySelector('.exerciseContainer').scrollBy(0, 100)"><img alt="Arrow Down Icon" src="/public/assets/icons/down.svg"></button>
 
 </div>
 
