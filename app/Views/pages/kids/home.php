0b9034c81bd8b48d5703a826240e20f6cc233056
loicr <loic.rondou@student.kuleuven.be>
2023-01-29 16:15:53 +0100

    problem solved on exercise, home and feedback screens

--- app/Views/pages/kids/home.php
+++ app/Views/pages/kids/home.php
@@ -15,8 +15,7 @@
         ?>
     </h1>
 
-    <div class="card_home_child">
-        <h3 class="three"> Your next exercise is waiting for you!</h3>
+
 
     <!--    <div class="start_ex_button">-->
         <?php foreach ($exercises as $ex):
@@ -28,23 +27,35 @@
                 endif;
             endforeach;
 
-            if($found == 0): ?>
-            <div class="start_ex_button">
-                    <h4 class="exercise_name" style="color:var(--primary-dark); text-transform: none;"> <span style="text-transform: none;" class="six">Lesson: </span> <?=$ex->lesson;?></h4>
-                    <h4 class="exercise_name" style="color:var(--primary-dark); text-transform: none;"> <span style="text-transform: none;" class="seven">Exercise:  </span> <?=$ex->name;?></h4>
-            </div>
-            <form action="<?php echo base_url('/kids/intro/'.$ex->idExercises);?>" class="start_ex_button" >
-                <button class="button buttonPrimary buttonChild four" >Start new exercise</button>
-            </form>
-            <?php break;
+            if($found == 0 && $ex->lesson!=0 ): ?>
+                <div class="card_home_child">
+                    <h3 class="three"> Your next exercise is waiting for you!</h3>
+                    <div class="start_ex_button">
+                                <h4 class="exercise_name" style="color:var(--primary-dark); text-transform: none;"> <span style="text-transform: none;" class="six">Lesson: </span> <?=$ex->lesson;?></h4>
+                                <h4 class="exercise_name" style="color:var(--primary-dark); text-transform: none;"> <span style="text-transform: none;" class="seven">Exercise:  </span> <?=$ex->name;?></h4>
+                        </div>
+                        <form action="<?php echo base_url('/kids/intro/'.$ex->idExercises);?>" class="start_ex_button" >
+                            <button class="button buttonPrimary buttonChild four" >Start new exercise</button>
+                        </form>
+                        <?php break;
+            elseif($found == 0 && $ex->lesson==0):?>
+                <div class="card_home_child">
+                    <h3 class="three"> Your next exercise is waiting for you!</h3>
+                    <div class="start_ex_button">
+                        <h4 class="exercise_name" style="color:var(--primary-dark); text-transform: none;"> <span style="text-transform: none;" class="six">Lesson: </span>Custom exercises</h4>
+                        <h4 class="exercise_name" style="color:var(--primary-dark); text-transform: none;"> <span style="text-transform: none;" class="seven">Exercise:  </span> <?=$ex->name;?></h4>
+                    </div>
+                    <form action="<?php echo base_url('/kids/intro/'.$ex->idExercises);?>" class="start_ex_button" >
+                        <button class="button buttonPrimary buttonChild four" >Start new exercise</button>
+                    </form>
+                    <?php break;
             endif;
             $found =1;
         endforeach;
             if($found ==1): ?>
-                <button class="button buttonSecondary buttonChild five" style="color:var(--blueNeutral); text-transform: none;">Congratulations, you have finished the game!</button>
-                <form action="<?php echo base_url('/kids/intro/1');?>" class="inline">
-                    <button class="button buttonPrimary buttonChild four">Start new exercise</button>
-                </form>
+                <div class="card_home_child">
+                    <h3 class="three"> Congratulations!</h3>
+                    <h3 class="three"> You finished the game</h3>
             <?php endif;?>
     <!--    </div>-->
     </div>
